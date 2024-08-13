c9676256ece5d38d44baab9aaf7d1b2df9cef68a
janos.kiss <kiss@jegymester.hu>
2024-08-13 16:31:07 +0200

    20240813 - Application initial version.

--- app/src/main/java/com/kissj/notes/activities/CreateNoteActivity.kt
+++ app/src/main/java/com/kissj/notes/activities/CreateNoteActivity.kt
@@ -1,26 +1,34 @@
 package com.kissj.notes.activities
 
 import android.content.Intent
+import android.content.res.ColorStateList
+import android.graphics.Color
+import android.graphics.drawable.GradientDrawable
 import android.os.AsyncTask
 import android.os.Build
 import android.os.Bundle
+import android.view.View
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.RequiresApi
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.kissj.notes.R
 import com.kissj.notes.database.NotesDatabase
 import com.kissj.notes.entities.Note
 import java.time.LocalDate
 
 class CreateNoteActivity : ComponentActivity() {
-    lateinit var inputNoteTitle: EditText
-    lateinit var inputNoteSubtitle: EditText
-    lateinit var inputNote: EditText
-    lateinit var textDateTime: TextView
+    private lateinit var inputNoteTitle: EditText
+    private lateinit var inputNoteSubtitle: EditText
+    private lateinit var inputNote: EditText
+    private lateinit var textDateTime: TextView
+    private lateinit var viewSubtitleIndicator: View
+    private var selectedNoteColor = "#333333"
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -47,12 +55,15 @@ class CreateNoteActivity : ComponentActivity() {
         inputNoteSubtitle = findViewById(R.id.inputNoteSubtitle)
         inputNote = findViewById(R.id.inputNote)
         textDateTime = findViewById(R.id.textDateTime)
+        viewSubtitleIndicator = findViewById(R.id.viewSubtitleIndicator)
 
         textDateTime.setText(LocalDate.now().toString())
 
+        initMiscellaneous()
+        setSubtitleIndicatorColor()
     }
 
-    fun saveNote() {
+    private fun saveNote() {
         if (inputNoteTitle.getText().isNullOrBlank()) {
             Toast.makeText(this, "Note title is mandatory", Toast.LENGTH_SHORT).show()
             return
@@ -67,7 +78,8 @@ class CreateNoteActivity : ComponentActivity() {
             title = inputNoteTitle.text.toString(),
             subTitle = inputNoteSubtitle.text.toString(),
             dateTime = textDateTime.text.toString(),
-            noteText = inputNote.text.toString()
+            noteText = inputNote.text.toString(),
+            color = selectedNoteColor
         )
 
         class SaveTask : AsyncTask<Void, Void, Void>() {
@@ -87,5 +99,81 @@ class CreateNoteActivity : ComponentActivity() {
         }
 
         SaveTask().execute()
+    }
+
+    private fun initMiscellaneous() {
+        val layoutMiscellaneous: LinearLayout = findViewById(R.id.layoutMiscellaneous)
+        val bottomSheetBehaviour: BottomSheetBehavior<LinearLayout> =
+            BottomSheetBehavior.from(layoutMiscellaneous)
+        layoutMiscellaneous.findViewById<TextView>(R.id.textMiscellaneous).setOnClickListener {
+            if (bottomSheetBehaviour.state != BottomSheetBehavior.STATE_EXPANDED) {
+                bottomSheetBehaviour.state = BottomSheetBehavior.STATE_EXPANDED
+            } else {
+                bottomSheetBehaviour.state = BottomSheetBehavior.STATE_COLLAPSED
+            }
+        }
+
+        val imageColor1 = layoutMiscellaneous.findViewById<ImageView>(R.id.imageColor1)
+        val imageColor2 = layoutMiscellaneous.findViewById<ImageView>(R.id.imageColor2)
+        val imageColor3 = layoutMiscellaneous.findViewById<ImageView>(R.id.imageColor3)
+        val imageColor4 = layoutMiscellaneous.findViewById<ImageView>(R.id.imageColor4)
+        val imageColor5 = layoutMiscellaneous.findViewById<ImageView>(R.id.imageColor5)
+
+        layoutMiscellaneous.findViewById<View>(R.id.viewColor1).setOnClickListener {
+            selectedNoteColor = "#333333"
+            imageColor1.setImageResource(R.drawable.ic_done)
+            imageColor2.setImageResource(0)
+            imageColor3.setImageResource(0)
+            imageColor4.setImageResource(0)
+            imageColor5.setImageResource(0)
+            setSubtitleIndicatorColor()
+        }
+
+        layoutMiscellaneous.findViewById<View>(R.id.viewColor2).setOnClickListener {
+            selectedNoteColor = "#fdbe3b"
+            imageColor1.setImageResource(0)
+            imageColor2.setImageResource(R.drawable.ic_done)
+            imageColor3.setImageResource(0)
+            imageColor4.setImageResource(0)
+            imageColor5.setImageResource(0)
+            setSubtitleIndicatorColor()
+        }
+
+        layoutMiscellaneous.findViewById<View>(R.id.viewColor3).setOnClickListener {
+            selectedNoteColor = "#ff4842"
+            imageColor1.setImageResource(0)
+            imageColor2.setImageResource(0)
+            imageColor3.setImageResource(R.drawable.ic_done)
+            imageColor4.setImageResource(0)
+            imageColor5.setImageResource(0)
+            setSubtitleIndicatorColor()
+        }
+
+        layoutMiscellaneous.findViewById<View>(R.id.viewColor4).setOnClickListener {
+            selectedNoteColor = "#3a52fc"
+            imageColor1.setImageResource(0)
+            imageColor2.setImageResource(0)
+            imageColor3.setImageResource(0)
+            imageColor4.setImageResource(R.drawable.ic_done)
+            imageColor5.setImageResource(0)
+            setSubtitleIndicatorColor()
+        }
+
+        layoutMiscellaneous.findViewById<View>(R.id.viewColor5).setOnClickListener {
+            selectedNoteColor = "#000000"
+            imageColor1.setImageResource(0)
+            imageColor2.setImageResource(0)
+            imageColor3.setImageResource(0)
+            imageColor4.setImageResource(0)
+            imageColor5.setImageResource(R.drawable.ic_done)
+            setSubtitleIndicatorColor()
+        }
+
+    }
+
+    private fun setSubtitleIndicatorColor() {
+        val gradientDrawable: GradientDrawable =
+            viewSubtitleIndicator.background as GradientDrawable
+        gradientDrawable.setColor(Color.parseColor(selectedNoteColor))
     }
 }
--- app/src/main/java/com/kissj/notes/adapters/NotesAdapter.kt
+++ app/src/main/java/com/kissj/notes/adapters/NotesAdapter.kt
@@ -1,8 +1,10 @@
 package com.kissj.notes.adapters
 
+import android.graphics.drawable.GradientDrawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.kissj.notes.R
@@ -11,12 +13,14 @@ import com.kissj.notes.entities.Note
 class NotesAdapter(val notes: List<Note>) : RecyclerView.Adapter<NotesAdapter.NotesViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NotesViewHolder {
-        return NotesViewHolder(LayoutInflater.from(parent.context)
-            .inflate(R.layout.item_container_note, parent, false))
+        return NotesViewHolder(
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_container_note, parent, false)
+        )
     }
 
     override fun onBindViewHolder(holder: NotesViewHolder, position: Int) {
-        holder.setNote(notes.get(position))
+        holder.setNote(notes[position])
     }
 
     override fun getItemCount(): Int {
@@ -28,20 +32,27 @@ class NotesAdapter(val notes: List<Note>) : RecyclerView.Adapter<NotesAdapter.No
     }
 
     class NotesViewHolder : RecyclerView.ViewHolder {
-        lateinit var textTitle: TextView
-        lateinit var textSubtitle: TextView
-        lateinit var textDateTime: TextView
+        val textTitle: TextView
+        val textSubtitle: TextView
+        val textDateTime: TextView
+        val linearLayout: LinearLayout
 
         constructor(itemView: View) : super(itemView) {
             textTitle = itemView.findViewById(R.id.textTitle)
             textSubtitle = itemView.findViewById(R.id.textSubtitle)
             textDateTime = itemView.findViewById(R.id.textDateTime)
+            linearLayout = itemView.findViewById(R.id.layoutNote)
         }
 
         fun setNote(note: Note) {
             textTitle.setText(note.title)
             textSubtitle.setText(note.subTitle)
             textDateTime.setText(note.dateTime)
+
+            if (note.color.isNotBlank()) {
+                val gradientDrawable = linearLayout.background as GradientDrawable
+                gradientDrawable.setColor(android.graphics.Color.parseColor(note.color))
+            }
         }
     }
 }
